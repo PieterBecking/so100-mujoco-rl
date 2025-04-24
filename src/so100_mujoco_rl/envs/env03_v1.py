959cba03ba87ddb4a5fc84b4608ab2211573188e
Lachlan Hurst <lachlan.hurst@gmail.com>
2025-04-24 11:57:38 +1000

    block now moves about in an ever increasing space at increasing speed

--- src/so100_mujoco_rl/envs/env03_v1.py
+++ src/so100_mujoco_rl/envs/env03_v1.py
@@ -18,6 +18,18 @@ from so100_mujoco_rl.envs.utils import JOINT_STEP_SCALE, MUJOCO_SO100_PREFIX
 
 START_POSITION = [0.0, -2.04, 1.19, 1.5, -1.58, 0.5]
 
+# min xyz, and max xyz
+BLOCK_SPACE_START = [
+    [-0.05, -0.35, 0.01],
+    [0.05, -0.25, 0.01]
+]
+BLOCK_SPACE_END = [
+    [-0.35, -0.45, 0.01],
+    [0.35, -0.15, 0.01]
+]
+BLOCK_SPEED_MIN = 0.2
+BLOCK_SPEED_MAX = 3.0
+
 END_CAM_RES_WIDTH = 1080
 END_CAM_RES_HEIGHT = 1920
 
@@ -84,6 +96,16 @@ class Env03(So100BaseEnv):
         self.cached_ob_center_x = -1.0
         self.cached_ob_center_y = -1.0
 
+        self.block_space_min = BLOCK_SPACE_START[0]
+        self.block_space_max = BLOCK_SPACE_START[1]
+        self.block_speed = BLOCK_SPEED_MIN
+        # initial block target is the center of the block space
+        self.block_target = [
+            (BLOCK_SPACE_START[0][i] + BLOCK_SPACE_START[1][i]) / 2 for i in range(3)
+        ]
+        self.block_target_dt = 0.01
+        self.block_target_time = 0.0
+
         self.offscreen_viewer = EndCamOffScreenViewer(
             width=END_CAM_RES_WIDTH,
             height=END_CAM_RES_HEIGHT,
@@ -119,7 +141,70 @@ class Env03(So100BaseEnv):
     def render(self):
         return super().render()
 
+    def _update_block_space(self, sim_time_fraction: float):
+        # Interpolate box_space_min and box_space_max based on sim_time_fraction
+        self.block_space_min = [
+            BLOCK_SPACE_START[0][i] + sim_time_fraction * (BLOCK_SPACE_END[0][i] - BLOCK_SPACE_START[0][i])
+            for i in range(3)
+        ]
+        self.block_space_max = [
+            BLOCK_SPACE_START[1][i] + sim_time_fraction * (BLOCK_SPACE_END[1][i] - BLOCK_SPACE_START[1][i])
+            for i in range(3)
+        ]
+
+    def _update_block_speed(self, sim_time_fraction: float):
+        # Interpolate block speed based on sim_time_fraction
+        if sim_time_fraction <= 0.05:
+            self.block_speed = BLOCK_SPEED_MIN
+        else:
+            self.block_speed = BLOCK_SPEED_MIN + (sim_time_fraction - 0.05) * (BLOCK_SPEED_MAX - BLOCK_SPEED_MIN) / (1.0 - 0.05)
+
+    def _update_block_target(self):
+        # we also need to check that the block hasn't already reached the target, if it has
+        # then it needs a new target otherwise it will just sit there
+        current_block_pos = self.data.joint('block_a_joint').qpos[0:3]
+        distance_to_target = np.linalg.norm(np.array(self.block_target) - np.array(current_block_pos))
+
+        if self.data.time - self.block_target_time < self.block_target_dt and distance_to_target > 0.02:
+            # still moving towards the last target
+            return
+        # Get a random block target position within block_space_min and block_space_max
+        target_x = np.random.uniform(self.block_space_min[0], self.block_space_max[0])
+        target_y = np.random.uniform(self.block_space_min[1], self.block_space_max[1])
+        target_z = np.random.uniform(self.block_space_min[2], self.block_space_max[2])
+
+        self.block_target = [target_x, target_y, target_z]
+        self.block_target_dt = np.random.uniform(1.2, 5.1)
+        self.block_target_time = self.data.time
+
+    def _update_block_position(self):
+        current_block_pos = self.data.joint('block_a_joint').qpos[0:3]
+
+        # Calculate the direction vector from current_block_pos to block_target
+        direction = np.array(self.block_target) - np.array(current_block_pos)
+        distance = np.linalg.norm(direction)
+
+        if distance > 0:
+            # Normalize the direction vector
+            direction = direction / distance
+
+            # Calculate the new position based on block_speed
+            step_distance = self.block_speed * self.model.opt.timestep
+            new_block_pos = np.array(current_block_pos) + direction * min(step_distance, distance)
+
+            # Update the block position
+            self.data.joint('block_a_joint').qpos[0:3] = new_block_pos
+
     def step(self, a):
+        # fraction increases from 0 to 1 over 6 seconds, doesn't exceed 1.0
+        sim_time_fraction = self.data.time / 6.0
+        sim_time_fraction = min(sim_time_fraction, 1.0)
+
+        self._update_block_space(sim_time_fraction)
+        self._update_block_speed(sim_time_fraction)
+        self._update_block_target()
+        self._update_block_position()
+
         joint_angles = self.get_joint_angles()
         new_joint_angles = [
             joint_angles[i] + a[i] * JOINT_STEP_SCALE for i in range(len(joint_angles))
@@ -127,7 +212,6 @@ class Env03(So100BaseEnv):
 
         for joint, new_angle in zip(self.joints, new_joint_angles):
             self.data.actuator(MUJOCO_SO100_PREFIX + joint.name).ctrl = new_angle
-
 
         mujoco.mj_step(self.model, self.data, nstep=self.frame_skip)
         mujoco.mj_rnePostConstraint(self.model, self.data)
